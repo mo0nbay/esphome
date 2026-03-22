357b7efdbe248ef3f0f3c44ec2fba5ea59e44fd3
rbaron <rbaron@rbaron.net>
2026-03-22 09:46:54 +0100

    Fix off by one error

--- esphome/components/broser_flipdot/broser_flipdot.cpp
+++ esphome/components/broser_flipdot/broser_flipdot.cpp
@@ -81,12 +81,7 @@ struct FlipCommands {
   uint8_t row;
 };
 
-int compute_flip_commands(int x, int y, bool on_off, FlipCommands &cmds) {
-  if (x < 0 || x >= MODULE_WIDTH || y < 0 || y >= MODULE_HEIGHT) {
-    ESP_LOGE(TAG, "Invalid coordinates for flip command: (%d, %d)", x, y);
-    return -1;
-  }
-
+void compute_flip_commands(int x, int y, bool on_off, FlipCommands &cmds) {
   // To set a dot, we set the column to - and row to +.
   // To reset a dot, we set the column to - and row to -.
 
@@ -94,19 +89,22 @@ int compute_flip_commands(int x, int y, bool on_off, FlipCommands &cmds) {
   // xy => B1 B0 A2 A1 A0
   //  i =>  4  3  2  1  0
 
-  // We add 1 because internally row/col 0 is mapped to A0 == 1. If A0 == A1 == A2 == 0, no row/col is selected.
-  const uint8_t xmod = x % MODULE_WIDTH + 1;
-  const uint8_t ymod = y % 14 + 1;
+  const uint8_t col_b = (x % MODULE_WIDTH) / 7;
+  const uint8_t col_a = (x % 7) + 1;
 
-  cmds.col = ((xmod >> 4) & 1) << 1     // COL_B1.
-             | ((xmod >> 3) & 1) << 2   // COL_B0.
-             | ((xmod >> 2) & 1) << 3   // COL_A2.
-             | ((xmod >> 1) & 1) << 4   // COL_A1.
-             | ((xmod >> 0) & 1) << 5   // COL_A0.
-             | (!on_off) << 6           // COLDAT.
-             | ((ymod >> 2) & 1) << 7;  // ROW_A2.
+  const uint8_t y_local = y % 14;
+  const uint8_t row_b = y_local / 7;
+  const uint8_t row_a = (y_local % 7) + 1;
 
-  // To turn on a pixel in row 0-13:
+  cmds.col = ((col_b >> 1) & 1) << 1     // COL_B1.
+             | ((col_b >> 0) & 1) << 2   // COL_B0.
+             | ((col_a >> 2) & 1) << 3   // COL_A2.
+             | ((col_a >> 1) & 1) << 4   // COL_A1.
+             | ((col_a >> 0) & 1) << 5   // COL_A0.
+             | (!on_off) << 6            // COLDAT.
+             | ((row_a >> 2) & 1) << 7;  // ROW_A2.
+
+  // E.g.: To turn on a pixel in row 0-13:
   //  Row set: 0A - 0G, 2A - 2G (total: 14 rows)
   //  Row reset: 1A - 1G, 3A - 3G (total: 14 rows)
   // The B1 Address controls maps to which half (0-7 or 8-15) of rows are affected
@@ -122,25 +120,24 @@ int compute_flip_commands(int x, int y, bool on_off, FlipCommands &cmds) {
   // ROW_0-13_EN = y < 14
   // ROW_14-27_DAT = on_off
   // ROW_14-27_EN = y >= 14
-
-  cmds.row = ((ymod >> 1) & 1) << 0    // ROW_A1.
-             | ((ymod >> 0) & 1) << 1  // ROW_A0.
-             | ((ymod > 7) & 1) << 2   // ROW_B1.
-             | (!on_off) << 3          // ROW_B0.
-             | on_off << 4             // ROW_0-13_DAT.
-             | (y < 14) << 5           // ROW_0-13_EN.
-             | on_off << 6             // ROW_14-27_DAT.
-             | (y >= 14) << 7;         // ROW_14-27_EN.
-
-  return 0;
+  cmds.row = ((row_a >> 1) & 1) << 0    // ROW_A1.
+             | ((row_a >> 0) & 1) << 1  // ROW_A0.
+             | (row_b & 1) << 2         // ROW_B1.
+             | (!on_off) << 3           // ROW_B0.
+             | on_off << 4              // ROW_0-13_DAT.
+             | (y < 14) << 5            // ROW_0-13_EN.
+             | on_off << 6              // ROW_14-27_DAT.
+             | (y >= 14) << 7;          // ROW_14-27_EN.
 }
 
 void BroserFlipdot::send_flip_command(int x, int y, bool on) {
-  FlipCommands cmds;
-  if (compute_flip_commands(x, y, on, cmds) != 0) {
-    ESP_LOGE(TAG, "Failed to compute flip commands for (%d, %d)", x, y);
+  if (x < 0 || x >= MODULE_WIDTH * this->num_chips_ || y < 0 || y >= MODULE_HEIGHT) {
+    ESP_LOGE(TAG, "Invalid coordinates for flip command: (%d, %d)", x, y);
     return;
   }
+
+  FlipCommands cmds;
+  compute_flip_commands(x, y, on, cmds);
 
   // Disable module.
   this->address_ = kI2CAddrMod;
